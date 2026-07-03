e5fbf70022c70cd4ac4000a697084424c4c9fea8
Gareis Pablo <gareispablotecnica1@gmail.com>
2026-07-03 16:57:33 -0300

    7.4

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+
+import Logo from './assets/Productos/a.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Titulo}>Producto1</Text>
+
+      <Image
+        source={Logo}
+        style={styles.ImgProducto}
+      ></Image>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    elevation:10,
+  },
+  ImgProducto:{
+    width:250,
+    height:250,
+    resizeMode:'contain',
+  },
+  Titulo:{
+    textAlign:'center',
+    fontSize:30,
+    margin:10,
+    color:'#304080',
   },
 });
